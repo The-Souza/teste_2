a006c494a6434b0d629f699d0e8cf253a6399190
Guilherme Campos <guilhermev272@gmail.com>
2024-09-23 13:26:43 -0300

    Update 3_vetor.py

--- 3_vetor.py
+++ 3_vetor.py
@@ -15,15 +15,15 @@ for i in dados:
 l3 = list(zip(l1, l2))
     
 print(l3)
+print()
 
 menor_valor = min(l2)
 maior_valor = max(l2)
 
-print(f'Maior faturamento = R${maior_valor}')
-print(f'Menor faturamento = R${menor_valor}')
+print(f'Maior faturamento: R${maior_valor}')
+print(f'Menor faturamento: R${menor_valor}')
 
 media = sum(l2) / len(l2)    
 dias_acima_da_media = len([f for f in l2 if f > media])
 
-print(media)
 print('Quantidade de dias que tiveram o faturamento acima da média: ',dias_acima_da_media)
